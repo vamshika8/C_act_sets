6688a8f1af6819d1984622baf4d347b81954c8f6
vamshika8 <145113792+vamshika8@users.noreply.github.com>
2024-03-03 19:31:01 +0530

    test

--- set02/problem06.c
+++ set02/problem06.c
@@ -7,35 +7,40 @@ void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 ```*/
 
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
 void input_string(char *a);
 void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
- int main()
- {
 
+int main() {
+    char original_string[100];
+    char reversed_string[100];
 
- }
+    input_string(original_string);
+    str_reverse(original_string, reversed_string);
+    output(original_string, reversed_string);
 
-
-void input_string(char *a)
-{
-    printf("Enter the string \n");
-    scanf("%s", a)
-
-
+    return 0;
 }
 
-
-void str_reverse(char *str, char *rev_str)
-{
-
-
+void input_string(char *a) {
+    printf("Enter a string: ");
+    scanf("%s", a);
 }
 
-void output(char *a, char *reverse_a)
-{
+void str_reverse(char *str, char *rev_str) {
+    int length = strlen(str);
+    int i, j;
 
+    for (i = length - 1, j = 0; i >= 0; i--, j++) {
+        rev_str[j] = str[i];
+    }
+    rev_str[j] = '\0';
+}
 
-
+void output(char *a, char *reverse_a) {
+    printf("Original String: %s\n", a);
+    printf("Reversed String: %s\n", reverse_a);
 }
